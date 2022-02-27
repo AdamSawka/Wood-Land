509287812b8c4d117874c5ea7a83d72f89cbb404
Adam Sawka <adam.sawka.net@gmail.com>
2022-02-27 14:30:54 +0100

    Added new styles nad images

--- js/landingPage/mainPage.js
+++ js/landingPage/mainPage.js
@@ -1,38 +1,40 @@
 import React from "react";
+import {Footer} from "./footer";
+import {Carousel1} from "./carousel";
+import * as url from "url";
 
 const MainPage = () => {
     return (
         <>
-            <header className="bg-dark py-5">
-                <div className="container px-4 px-lg-5 my-5">
+            <header className=" bg-image py-5 text-center shadow-2-strong rounded mb-5 text-white header-mainPage">
+
+                <div className="container px-4 px-lg-5 my-5 mainPage-opacity">
                     <div className="text-center text-white">
-                        <h1 className="display-4 fw-bolder">Shop in style</h1>
-                        <p className="lead fw-normal text-white-50 mb-0">With this shop hompeage template</p>
+                        <h1 className="display-6 fw-bolder"> Twoje dekoracje w zgodzie z naturą </h1>
                     </div>
                 </div>
             </header>
-
+        <Carousel1/>
             <section className="py-5">
                 <div className="container px-4 px-lg-5 mt-5">
                     <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
                         <div className="col mb-5">
                             <div className="card h-100">
 
-                                <img className="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg"
+                                <img className="card-img-top" src="../../img/deer1.jpg"
                                      alt="..."/>
 
                                 <div className="card-body p-4">
                                     <div className="text-center">
 
-                                        <h5 className="fw-bolder">Fancy Product</h5>
+                                        <h5 className="fw-bolder">Popularne produkty</h5>
 
-                                        $40.00 - $80.00
+                                        PLN 40.00 - PLN 80.00
                                     </div>
                                 </div>
 
                                 <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                                    <div className="text-center"><a className="btn btn-outline-dark mt-auto" href="#">View
-                                        options</a></div>
+                                    <div className="text-center"><a className="btn btn-outline-dark mt-auto" href="#">Więcej </a></div>
                                 </div>
                             </div>
                         </div>
@@ -40,28 +42,27 @@ const MainPage = () => {
                             <div className="card h-100">
 
                                 <div className="badge bg-dark text-white position-absolute"
-                                     >Sale
+                                     >Wyprzedaż
                                 </div>
 
-                                <img className="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg"
+                                <img className="card-img-top" src="../../img/draw1.jpg"
                                      alt="..."/>
 
                                 <div className="card-body p-4">
                                     <div className="text-center">
 
-                                        <h5 className="fw-bolder">Special Item</h5>
+                                        <h5 className="fw-bolder">Okolicznościowe</h5>
 
                                         <div className="d-flex justify-content-center small text-warning mb-2">
                                         </div>
 
-                                        <span className="text-muted text-decoration-line-through">$20.00</span>
-                                        $18.00
+                                        <span className="text-muted text-decoration-line-through">PLN 20.00</span>
+                                         PLN 18.00
                                     </div>
                                 </div>
 
-                                <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                                    <div className="text-center"><a className="btn btn-outline-dark mt-auto" href="#">Add
-                                        to cart</a></div>
+                                <div className="card-footer ">
+                                    <div className="text-center"><a className="btn btn-outline-dark mt-auto" href="#">Dodaj do koszyka</a></div>
                                 </div>
                             </div>
                         </div>
@@ -69,8 +70,9 @@ const MainPage = () => {
                     </div>
                 </div>
             </section>
-
+            <Footer/>
         </>
+
     )
 }
 export {MainPage};
--- js/landingPage/navigation.js
+++ js/landingPage/navigation.js
@@ -5,7 +5,7 @@ const Navigation = () => {
         <>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <div className="container px-4 px-lg-5">
-                    <a className="navbar-brand" href="#">Start Bootstrap</a>
+                    <a className="navbar-brand" href="#">Wood Land</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
                             data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                             aria-expanded="false" aria-label="Toggle navigation"> <span
@@ -13,25 +13,26 @@ const Navigation = () => {
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
                             <li className="nav-item"><a className="nav-link active" aria-current="page"
-                                                        href="#">Home</a></li>
-                            <li className="nav-item"><a className="nav-link" href="#">About</a></li>
+                                                        href="#">Strona główna</a></li>
+                            <li className="nav-item"><a className="nav-link" href="#">O nas</a></li>
+                            <li className="nav-item"><a className="nav-link" href="#">Kontakt</a></li>
                             <li className="nav-item dropdown">
                                 <a className="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button"
-                                   data-bs-toggle="dropdown" aria-expanded="false">Shop</a>
+                                   data-bs-toggle="dropdown" aria-expanded="false">Sklep</a>
                                 <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <li><a className="dropdown-item" href="#">All Products</a></li>
+                                    <li><a className="dropdown-item" href="#">Wszystkie produkty</a></li>
                                     <li>
                                         <hr className="dropdown-divider"/>
                                     </li>
-                                    <li><a className="dropdown-item" href="#">Popular Items</a></li>
-                                    <li><a className="dropdown-item" href="#">New Arrivals</a></li>
+                                    <li><a className="dropdown-item" href="#">Popularne</a></li>
+                                    <li><a className="dropdown-item" href="#">Nowości</a></li>
                                 </ul>
                             </li>
                         </ul>
                         <form className="d-flex">
                             <button className="btn btn-outline-dark" type="submit">
                                 <i className="bi-cart-fill me-1"> </i>
-                                Cart
+                                Koszyk
                                 <span className="badge bg-dark text-white ms-1 rounded-pill">0</span>
                             </button>
                         </form>
