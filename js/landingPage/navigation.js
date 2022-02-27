91114c1d9942306c0e8a4684dfcd9175423d822c
Adam Sawka <adam.sawka.net@gmail.com>
2022-02-27 22:21:09 +0100

    Added collapse menu

--- js/landingPage/navigation.js
+++ js/landingPage/navigation.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
+
 
 const Navigation = () => {
+    const [isOpen, setIsOpen] = useState(false);
+     const toggleOpen = () => setIsOpen( (prev) => prev = !isOpen );
+    const menuClass = `dropdown-menu${isOpen ? "-show" : ""}`;
     return (
         <>
+
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <div className="container px-4 px-lg-5">
-                    <img src="../../img/logo_size.jpg" alt="logo"/> <a className="navbar-brand" href="../../index.html">Wood Land</a>
+                     <a className="navbar-brand" href="../../index.html"><img src="../../img/logo.png" alt="logo" className="mx-2 bg-light"/></a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
                             data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                             aria-expanded="false" aria-label="Toggle navigation"> <span
@@ -14,20 +19,20 @@ const Navigation = () => {
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
                             <li className="nav-item"><a className="nav-link active" aria-current="page"
                                                         href="#">Strona główna</a></li>
-                            <li className="nav-item"><a className="nav-link" href="#">O nas</a></li>
-                            <li className="nav-item"><a className="nav-link" href="#">Kontakt</a></li>
-                            <li className="nav-item dropdown">
+                            <li className="nav-item mx-4"><a className="nav-link" href="#">O nas</a></li>
+                            <li className="nav-item mx-4"><a className="nav-link" href="#">Kontakt</a></li>
+                            <li className="nav-item mx-4 dropdown">
                                 <a className="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button"
-                                   data-bs-toggle="dropdown" aria-expanded="false">Sklep</a>
-                                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                   data-bs-toggle="dropdown" aria-expanded="false" onClick={toggleOpen}>Sklep</a>
+                                <ul className={menuClass}  aria-labelledby="navbarDropdown">
                                     <li><a className="dropdown-item" href="#">Wszystkie produkty</a></li>
-                                    <li>
-                                        <hr className="dropdown-divider"/>
-                                    </li>
                                     <li><a className="dropdown-item" href="#">Popularne</a></li>
                                     <li><a className="dropdown-item" href="#">Nowości</a></li>
                                 </ul>
                             </li>
+                        </ul>
+                        <ul className="navbar-nav mb-2 mb-lg-0 ms-lg-4">
+                        <li className="nav-item mx-4"><a className="nav-link" href="#">Zarejestruj się</a></li>
                         </ul>
                         <form className="d-flex">
                             <button className="btn btn-outline-dark" type="submit">
