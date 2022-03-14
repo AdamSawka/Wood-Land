2da1922070b747a0e31e779bd2d8a34bf5e4dfaa
Adam Sawka <adam.sawka.net@gmail.com>
2022-03-14 18:40:05 +0100

    Created information that basket is empty or not

--- src/landingPage/Basket.js
+++ src/landingPage/Basket.js
@@ -7,12 +7,19 @@ const Basket = ({items, add})=>{
     const products = Products()
     const Sorted = items.map(item=> products.filter(element => element.id === item))
    const SortedArray = Sorted.flat()
+
     return<>
-        <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 mx-1 justify-content-center margin-top">
-            {SortedArray.map(item =>(
-                <Item add={add}  name={item.name} img={item.img} price={item.price} id={item.id} />
-            ))}
-        </div>
+        {SortedArray.length > 0
+            ? <div className=" row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 mx-1 justify-content-center margin-top">
+                {SortedArray.map(item =>(
+                    <Item add={add}  name={item.name} img={item.img} price={item.price} id={item.id} />
+                ))}
+            </div>
+            : <div className="vh-100 d-flex align-items-center justify-content-center"><h1>Koszyk jest pusty</h1></div>
+
+        }
+
+
     </>
 }
 export {Basket}
