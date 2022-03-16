6a660105bdb481538233077ef93bc51870c98634
Adam Sawka <adam.sawka.net@gmail.com>
2022-03-16 09:39:51 +0100

    Added new styles for navigationSort section

--- src/landingPage/shop/itemsAll.js
+++ src/landingPage/shop/itemsAll.js
@@ -9,9 +9,9 @@ const ItemsAll = ({add}) => {
     return <>
                 <div className="container d-flex row w-100 mx-auto ">
                     <NavigationFilter />
-                    <div className=" col-sm-8 col-md-10 col-lg-10 col-xl-10">
+                    <div className=" col-sm-8 col-md-9 col-lg-10 col-xl-10">
                     <h1 className="d-block margin-top w-100 pt-3 text-center">Wszystkie produkty</h1>
-                    <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 mx-1 justify-content-center">
+                    <div className="row gx-4 gx-lg-5  row-cols-md-3 row-cols-xl-4 mx-1 justify-content-center">
                         {    products.map( item =>(
                         <Item add={add} id={item.id}  key={item.id} name={item.name} img={item.img} price={item.price}  />
                         ))}
--- src/landingPage/shop/navigationFilter.js
+++ src/landingPage/shop/navigationFilter.js
@@ -3,38 +3,38 @@ import React from "react";
 const NavigationFilter = () => {
 
     return <>
-        <div className="col-sm-4 col-md-2 col-lg-2 col-xl-2 vh-100 ">
-            <div className="d-flex flex-column align-items-center h-100 justify-content-center">
-                <p>Sortuj według:</p>
-                <div>
-                    <label className="mx-2" htmlFor="price">Cena</label>
-                    <input type="checkbox" id="price" name="price"/>
+        <div className="col-sm-4 col-md-3 col-lg-2 col-xl-2 vh-100 ">
+            <div className="d-flex flex-column align-items-center  h-100 justify-content-center">
+                <p className="border-bottom">Sortuj według:</p>
+                <div className="d-flex justify-content-between align-items-center w-100">
+                    <label className="mx-4" htmlFor="price">Cena</label>
+                    <input className="" type="checkbox" id="price" name="price"/>
                 </div>
 
-                <p className="mt-4 mb-1">Rodzaj</p>
+                <p className="mt-4 mb-2 border-bottom">Rodzaj:</p>
 
-                <div>
-                    <label className="mx-2" htmlFor="pillow">Poduszki</label>
+                <div className="d-flex justify-content-between align-items-center w-100">
+                    <label className="mx-4" htmlFor="pillow">Poduszki</label>
                     <input type="checkbox" id="pillow" name="pillow"/>
                 </div>
 
-                <div>
-                    <label className="mx-2" htmlFor="image">Obrazy</label>
+                <div className="d-flex justify-content-between align-items-center w-100">
+                    <label className="mx-4" htmlFor="image">Obrazy</label>
                     <input type="checkbox" id="image" name="image"/>
                 </div>
 
-                <div>
-                    <label className="mx-2" htmlFor="decoration">Ozdoby</label>
+                <div className="d-flex justify-content-between align-items-center w-100">
+                    <label className="mx-4" htmlFor="decoration">Ozdoby</label>
                     <input type="checkbox" id="decoration" name="decoration"/>
                 </div>
 
-                <div>
-                    <label className="mx-2" htmlFor="tshirt">Koszulki</label>
+                <div className="d-flex justify-content-between align-items-center w-100">
+                    <label className="mx-4" htmlFor="tshirt">Koszulki</label>
                     <input type="checkbox" id="tshirt" name="tshirt"/>
                 </div>
 
-                <div>
-                    <label className="mx-2" htmlFor="poster">Plakaty</label>
+                <div className="d-flex justify-content-between align-items-center w-100">
+                    <label className="mx-4" htmlFor="poster">Plakaty</label>
                     <input type="checkbox" id="poster" name="poster"/>
                 </div>
 
