985925d25b77ef60266c43520d6dd94d4fe12717
Adam Sawka <adam.sawka.net@gmail.com>
2022-03-17 19:22:31 +0100

    Created handleChange structure for NavigationSort section

--- src/landingPage/shop/navigationFilter.js
+++ src/landingPage/shop/navigationFilter.js
@@ -2,40 +2,45 @@ import React from "react";
 
 const NavigationFilter = () => {
 
+    const handleChange = ()=>{
+
+        return
+    }
+
     return <>
         <div className="col-sm-4 col-md-3 col-lg-2 col-xl-2 vh-100 ">
             <div className="d-flex flex-column align-items-center  h-100 justify-content-center">
                 <p className="border-bottom">Sortuj według:</p>
                 <div className="d-flex justify-content-between align-items-center w-100">
                     <label className="mx-4" htmlFor="price">Cena</label>
-                    <input className="" type="checkbox" id="price" name="price"/>
+                    <input onChange={handleChange.bind("price")} className="" type="checkbox" id="price" name="price"/>
                 </div>
 
                 <p className="mt-4 mb-2 border-bottom">Rodzaj:</p>
 
                 <div className="d-flex justify-content-between align-items-center w-100">
                     <label className="mx-4" htmlFor="pillow">Poduszki</label>
-                    <input type="checkbox" id="pillow" name="pillow"/>
+                    <input onChange={handleChange.bind("pillow")} type="checkbox" id="pillow" name="pillow"/>
                 </div>
 
                 <div className="d-flex justify-content-between align-items-center w-100">
                     <label className="mx-4" htmlFor="image">Obrazy</label>
-                    <input type="checkbox" id="image" name="image"/>
+                    <input  onChange={handleChange.bind("image")} type="checkbox" id="image" name="image"/>
                 </div>
 
                 <div className="d-flex justify-content-between align-items-center w-100">
                     <label className="mx-4" htmlFor="decoration">Ozdoby</label>
-                    <input type="checkbox" id="decoration" name="decoration"/>
+                    <input onChange={handleChange.bind("decoration")} type="checkbox" id="decoration" name="decoration"/>
                 </div>
 
                 <div className="d-flex justify-content-between align-items-center w-100">
                     <label className="mx-4" htmlFor="tshirt">Koszulki</label>
-                    <input type="checkbox" id="tshirt" name="tshirt"/>
+                    <input onChange={handleChange.bind("tshirt")} type="checkbox" id="tshirt" name="tshirt"/>
                 </div>
 
                 <div className="d-flex justify-content-between align-items-center w-100">
                     <label className="mx-4" htmlFor="poster">Plakaty</label>
-                    <input type="checkbox" id="poster" name="poster"/>
+                    <input onChange={handleChange.bind("poster")} type="checkbox" id="poster" name="poster"/>
                 </div>
 
             </div>
